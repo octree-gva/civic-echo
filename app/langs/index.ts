98bdea44dd6d0516a23808a5ab186021dba6ac36
Tim Izzo <tim@octree.ch>
2022-04-08 16:38:41 +0200

    chore(NextJS): 🌐 Add new langs

--- app/langs/index.ts
+++ app/langs/index.ts
@@ -1,8 +1,11 @@
 import fr from "./fr.yaml";
+import en from "./en.yaml";
+import de from "./de.yaml";
+import pt from "./pt.yaml";
 
-export const LANGS = ["fr", "en", "pt"];
+export const LANGS = ["fr", "en", "pt", "de"];
 
-const langs: LangContent[] = [fr];
+const langs: LangContent[] = [fr, en, de, pt];
 
 export const getLangContent = (targetLang: string) => {
   return langs.find(lang => lang.lang === targetLang);
