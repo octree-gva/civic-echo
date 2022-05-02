51e44a6687f78ab22d76706361f9e9c1f9c077cf
Tim Izzo <tim@octree.ch>
2022-05-02 18:08:14 +0200

    🔧 Set prod config

--- app/config/index.ts
+++ app/config/index.ts
@@ -2,11 +2,10 @@
 export const logoUrl = "https://demaincestaujourdhui.online";
 
 // Position of questions for personal data
-export const personalIndex = 3;
+export const personalIndex = 41;
 
 // Show survey as closed
 export const isClosed = false;
 
 // URL to survey results
-export const resultsUrl =
-  "http://metabase-8ee.hidora.com/public/dashboard/0ebfeed2-e9b3-4103-9a24-482b013b06e3";
+export const resultsUrl = "https://demaincestaujourdhui.online";
